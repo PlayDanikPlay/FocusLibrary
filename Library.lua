13e0c634ccfce19f78abc7e93c004f17f8b88b8e
PlayDanikPlay <127974459+PlayDanikPlay@users.noreply.github.com>
2025-04-13 18:37:40 +0300

    Create Library.lua

---
+++ Library.lua
@@ -0,0 +1,7 @@
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+ 
+local Player = Players.LocalPlayer
+local Mouse = Player:GetMouse()
